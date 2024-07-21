3f8ab09091235db5d4d9d8ce900060e0961ac060
Martin <martinkz@users.noreply.github.com>
2024-07-21 11:38:01 +0100

    Fix: animation speed

--- src/Quiz/MotionWrapper.tsx
+++ src/Quiz/MotionWrapper.tsx
@@ -60,7 +60,7 @@ export const MotionScale = {
 export const MotionSlideLeft = {
 	initial: { opacity: 0, x: "-120px" },
 	animate: { opacity: 1, x: 0 },
-	transition: { duration: 1.5 },
+	transition: { duration: 0.5 },
 	exit: { opacity: 0, x: "-120px" },
 };
 
